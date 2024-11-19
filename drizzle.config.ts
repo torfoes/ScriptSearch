57f315e932dc199eb3b121104ebb3f4ddda9b3bf
karlos <karloszuru@gmail.com>
2024-11-18 20:35:19 -0600

    changes to db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,9 +8,9 @@ export default defineConfig({
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_DATABASE,
+    host: process.env.DATABASE_HOST!,
+    port: Number(process.env.DATABASE_PORT),
+    database: process.env.DATABASE_DATABASE!,
     user: process.env.DATABASE_USER!,
     password: process.env.DATABASE_PASSWORD!,
     ssl: {
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,13 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+// eslint-disable-next-line
+export const db = drizzle(pool);
